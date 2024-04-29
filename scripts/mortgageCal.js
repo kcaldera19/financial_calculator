b9882f7281ce751ff51eb5d2833cbc65c5a9fb3f
Katalina Caldera <katalinacaldera19@gmail.com>
2024-04-29 16:30:46 -0500

    added formual to js

--- scripts/mortgageCal.js
+++ scripts/mortgageCal.js
@@ -7,12 +7,17 @@ function monthlyMorgagePayment() {
     let loanLengthField = parseFloat(document.querySelector("#loanLength").value);
 
     // converting annual interest rate to monthly by 12 * 100
-    let monthlyInterestRate = annualInterestRateValue / 12/100;
+    let monthlyInterestRate = annualInterestRateValue / 100/12;
     let numberfPayments = loanLengthField * 12;
+    const denominator = Math.pow(1 + monthlyInterestRate, numberfPayments);
+    const monthlyPayment = loanAmountField * (monthlyInterestRate * denominator) / (denominator - 1);
 
-    let monthlyPayment = (loanAmountField * monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, -numberfPayments));
+    const totalAmount = numberfPayments* monthlyPayment;
+    const totalInterest = totalAmount - loanAmountField;
+    
+    // let monthlyPayment = (loanAmountField * monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, -numberfPayments));
 
-    let total= `The monthly payment will be:$${monthlyPayment.toFixed(2)}`;
+    let total= `The monthly payment will be:$${monthlyPayment.toFixed(2)} total interest $${totalInterest.toFixed(2)}`;
     document.querySelector("#payment").innerHTML = total;
 }
  
