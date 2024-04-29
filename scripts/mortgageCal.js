515991149734daa97e8c20b5362ad3541e3fc8b5
Katalina Caldera <katalinacaldera19@gmail.com>
2024-04-29 13:19:13 -0500

    added button to html

--- scripts/mortgageCal.js
+++ scripts/mortgageCal.js
@@ -1,7 +1,19 @@
 "use strict"
 console.log("Work!");
-window.onload = init;
-function init() {
+window.onload = monthlyMorgagePayment;
+function monthlyMorgagePayment( ) {
+    <div>
+    Principal (loan amount):$ <input type="text"  type="number" id="principalAmount">
+</div>
+<div>
+    Monthly interest Rate: <input type="text" type="number" id="monthlyInterestRate">
+</div>
+<div>
+    Length of Loan in Months: <input type="text" type="number" id="loanLength">
+</div>
+<div>
+    $ <input type="text"  type="number" id="principalAmount">
+</div>
     
 
 }
