5867a303394810e0aa0a5a2aa9a18c1e56194ddf
Katalina Caldera <katalinacaldera19@gmail.com>
2024-04-29 15:33:27 -0500

    added js formula

--- scripts/mortgageCal.js
+++ scripts/mortgageCal.js
@@ -1,19 +1,17 @@
 "use strict"
-console.log("Work!");
-window.onload = monthlyMorgagePayment;
-function monthlyMorgagePayment( ) {
-    <div>
-    Principal (loan amount):$ <input type="text"  type="number" id="principalAmount">
-</div>
-<div>
-    Monthly interest Rate: <input type="text" type="number" id="monthlyInterestRate">
-</div>
-<div>
-    Length of Loan in Months: <input type="text" type="number" id="loanLength">
-</div>
-<div>
-    $ <input type="text"  type="number" id="principalAmount">
-</div>
-    
 
+window.onload = monthlyMorgagePayment;
+function monthlyMorgagePayment() {
+    const loanAmountField = parseFloat(document.querySelector("#loanAmount").value);
+    const annualInterestRateValue = parseFloat(document.querySelector("#annualnterestRate").value);
+    const loanLengthField = parseFloat(document.querySelector("#loanLength").value);
+
+    // converting annual interest rate to monthly by 12 * 100
+    const monthlyInterestRate = annualInterestRateValue / 1200;
+    const numberfPayments = loanLengthField * 12;
+
+    const monthlyPayment = (loanAmountField * monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, -numberfPayments));
+
+    const total= `The monthly payment will be:$${monthlyPayment.toFixed(2)}`;
+    document.querySelector("#payment").innerHTML = total;
 }
