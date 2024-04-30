dfdfc1ce200becf85322ce93b1e294614cb559f3
Katalina Caldera <katalinacaldera19@gmail.com>
2024-04-30 11:34:58 -0500

    future value

---
+++ scripts/futureValueCal.js
@@ -0,0 +1,29 @@
+"use strict"
+console.log("I work");
+
+window.onload = init
+
+function init(){
+
+    // document grabs the html page by the id
+    const futureValueForm = document.querySelector("#futureValueForm");
+
+    futureValueForm.addEventListener("submit", calcFutureValue);
+
+}
+// What happens when the user clicks on something
+function calcFutureValue(event){
+    event.preventDefault();
+    const deposit = Number(document.querySelector("#deposit").value);
+    const interestRate = Number(document.querySelector("#interestRate").value);
+    const years = Number(document.querySelector("#years").value);
+
+
+    // futureValue = desposit * (1 + interestRate/100) ** years;
+    const futureValue = deposit * (1 + interestRate) ** years;
+    const interestEarned = futureValue - deposit;
+
+    let resultDiv = document.querySelector("#results");
+
+    resultsDiv.innerHTML=` If you deposit $${deposit} in a cd that earns ${interestRate} `
+}
